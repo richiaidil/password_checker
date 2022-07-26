7eea1f44d3d1eaf0cddf7c67591c248dff507865
richiaidil <106723859+richiaidil@users.noreply.github.com>
2022-07-26 12:28:52 +0800

    Create body.py

---
+++ body.py
@@ -0,0 +1,49 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+
+st.write("""
+# PASSWORD CHECKER
+Are you cofident  that your password is **STRONG** or not?
+""")
+
+pw = st.text_input('Enter your password', '#HireMe1234')
+
+def pw_check(password)
+    point = 0
+    num = False
+    upper = False
+    lower = False
+    alphanum = False
+
+    for letter in password:
+        if letter.isnumeric():
+            num = True
+        elif letter.islower():
+            lower = True
+        elif letter.isupper():
+            upper = True
+        elif not letter.isalnum():
+            alphanum = True
+        if upper and lower and num and alphanum:
+            break
+    if lower == True:
+      point+=5
+    if lower == True and upper == True:
+      point+=10
+    if lower == True and num == True:
+      point+=10
+    if lower == True and alphanum == True:
+      point+=10
+    if len(pw) >= 8:
+      point+=5
+      
+      return point
+    
+st.subheader('Your Point')
+st.write(point)
+
+    
+    
+
